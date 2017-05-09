83789dd375b5219f7adb97e151e3dc503b06b6dc
Baban Kumar Sinha <sinha.baban@gmail.com>
2017-05-09 11:19:41 +0530

    Initial commit

---
+++ src/test/java/com/psl/tdd/util/FileReaderUtil.java
@@ -0,0 +1,14 @@
+package com.psl.tdd.util;
+
+import org.junit.Test;
+
+//import static com.jayway.restassured.RestAssured.*;
+//import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class FileReaderUtil {
+	@Test
+	public void firstEchoTest() {
+		//get("/echo/hello").then().assertThat().body("message", equalTo("hello"));
+	}
+}
