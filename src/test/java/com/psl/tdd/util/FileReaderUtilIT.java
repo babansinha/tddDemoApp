918cce9e3bb4c5ecb9452a52b0d2d8a3614da7dc
Baban <baban_sinha@persistent.co.in>
2017-05-09 12:29:35 +0530

    Refector test class name

--- src/test/java/com/psl/tdd/util/FileReaderUtilIT.java
+++ src/test/java/com/psl/tdd/util/FileReaderUtilIT.java
@@ -2,13 +2,14 @@ package com.psl.tdd.util;
 
 import org.junit.Test;
 
-//import static com.jayway.restassured.RestAssured.*;
+import static com.jayway.restassured.RestAssured.*;
 //import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
-public class FileReaderUtil {
+public class FileReaderUtilIT {
 	@Test
 	public void firstEchoTest() {
+		//get("/echo/hello").then().assertThat().body("message", equalTo("hello"));
 		//get("/echo/hello").then().assertThat().body("message", equalTo("hello"));
 	}
 }
