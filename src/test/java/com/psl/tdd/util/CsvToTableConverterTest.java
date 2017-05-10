8eb7b094c2b0330fd9d8109111d420bb4ca32ce2
ankitjha-1508 <ankit_jha@persistent.co.in>
2017-05-10 11:07:43 +0530

    @Author : Ankit, Adding test cases(TDD) for CsvToTableConverter

---
+++ src/test/java/com/psl/tdd/util/CsvToTableConverterTest.java
@@ -0,0 +1,25 @@
+package com.psl.tdd.util;
+
+public class CsvToTableConverterTest {
+	
+	public void testValidFile(){}
+	
+	public void testValidTableName(){}
+	public void testValidColumn(){}
+	
+	public void testForInvalidInput(){}
+	public void testForValidInput(){}
+	public void testTableWithNoHeader(){}
+	public void testTableWithMoreThanDesiredHeader(){}
+	
+	public void testTableWithHeaderOnly(){}
+	public void testTableWithOneColumnRow(){}
+	
+	public void testTableWithColumnNameOnly(){}
+	public void testTableWithColumnNameTypeOnly(){}
+	public void testTableWithColumnNameTypeLengthOnly(){}
+	public void testTableWithColumnNameTypeLengthAutoincrementOnly(){}
+	
+	public void testForValidInputToGetValidOutput(){}
+
+}
