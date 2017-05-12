81a76fe67f1187e870be9032154504b1beeab51e
babansinha <baban_sinha@persistent.co.in>
2017-05-12 12:12:47 +0530

    Added java doc and Cobertura code coverage plug-in

--- src/test/java/com/psl/tdd/util/FileReaderUtilIT.java
+++ src/test/java/com/psl/tdd/util/FileReaderUtilIT.java
@@ -13,35 +13,47 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * This Class is responsible to test file reading functionalities
+ *
+ */
 public class FileReaderUtilIT extends BaseTest {
 		
+	/**
+	 * This function is responsible to test NULL value as a directory path
+	 * This will throw ReaderException with {@link MessageConstant.INVALID_DIRECTORY_PATH} message
+	 * 
+	 * @throws ReaderException
+	 */
 	@Test
 	public void testNullDirectoryAsInput() throws ReaderException {
-		System.out.println("testNullDirectoryAsInput. . .");
 		thrown.expect(ReaderException.class);
         thrown.expectMessage(MessageConstant.INVALID_DIRECTORY_PATH);
         FileReaderUtil.getSchemaFileListFromSchemasFolder(null);
 	}
 	
+	/**
+	 * This function is responsible to test 'INVALID' value as a directory path
+	 * This will throw ReaderException with {@link MessageConstant.INVALID_DIRECTORY_PATH} message
+	 * 
+	 * @throws ReaderException
+	 */
 	@Test
 	public void testIsDirectoryExistWithInvalidPath() throws ReaderException {
-		System.out.println("testIsDirectoryExistWithInvalidPath. . .");
 		thrown.expect(ReaderException.class);
         thrown.expectMessage(MessageConstant.INVALID_DIRECTORY_PATH);
         FileReaderUtil.getSchemaFileListFromSchemasFolder(ConstantsUtil.INVALID_SCHEMA_FOLDER_ABSOLUTE_PATH);
 	}
 	
-	@Test
-	public void testIsDirectoryExistWithValidPath() throws ReaderException {
-		System.out.println("testIsDirectoryExistWithValidPath. . .");
-		
-        List<String> list = FileReaderUtil.getSchemaFileListFromSchemasFolder(ConstantsUtil.SCHEMA_FOLDER_ABSOLUTE_PATH);
-        assertTrue(list.size()>0);
-	}
 	
+	/**
+	 * This function is responsible to test whether file exist or not of given Empty directory
+	 * This will throw ReaderException with {@link MessageConstant.EMPTY_DIRECTORY} message
+	 * 
+	 * @throws ReaderException
+	 */
 	@Test
-	public void testIsFileExist() throws ReaderException, IOException {
-		System.out.println("testIsFileExist. . .");
+	public void testIsFileExistForEmptyDirectory() throws ReaderException, IOException {
 		thrown.expect(ReaderException.class);
 		thrown.expectMessage(MessageConstant.EMPTY_DIRECTORY);
 		File folder = testFolder.newFolder("FileReader_EmptyFolder");
@@ -49,19 +61,30 @@ public class FileReaderUtilIT extends BaseTest {
 	}
 	
 	
+	/**
+	 * This function is responsible to test whether file(it may be empty file) exist or not of given valid file path
+	 * This will throw ReaderException with {@link MessageConstant.EMPTY_FILE} message
+	 * 
+	 * @throws IOException, ReaderException
+	 */
 	@Test
-	public void testIsValidFile() throws IOException, ReaderException {
-		System.out.println("testIsValidFile. . .");
+	public void testIsValidForEmptyFile() throws IOException, ReaderException {
 		thrown.expect(ReaderException.class);
 		thrown.expectMessage(MessageConstant.EMPTY_FILE);
 		File emptyFile = testFolder.newFile("FileReader_EmptyFile");
 		FileReaderUtil.isValidFile(emptyFile);
-		
 	}
 	
-	/*@Test
-	public void testReadFilesFromDirectory() {
-		System.out.println("testReadFilesFromDirectory. . .");
-		// Combination of all above test cases
-	}*/
+	/**
+	 * This function is responsible to test file reading method with all valid scenario 
+	 * This will return list of file's absolute path
+	 * 
+	 * @throws ReaderException
+	 */
+	@Test
+	public void testReadFileWithValidInput() throws ReaderException {
+        List<String> list = FileReaderUtil.getSchemaFileListFromSchemasFolder(ConstantsUtil.SCHEMA_FOLDER_ABSOLUTE_PATH);
+        assertTrue(list.size() > 0);
+	}
+	
 }
